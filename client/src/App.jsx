c922b7891039b06ae27f149b7c631d988dd68b48
Alan Joy <alanjoy456@gmail.com>
2024-08-03 13:23:12 +0530

    pages and routes made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
 
 
 function App() {
   return (
-    <div>App</div>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/signin' element={<SignIn/>}/>
+      <Route path='/signout' element={<SignOut/>}/>
+    </Routes>
+   </BrowserRouter>
   )
 }
 
